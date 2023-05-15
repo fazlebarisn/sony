27233a30b2c33a383900ec7fd957cc642088c9f5
Fazle Bari <fazlebarisn@gmail.com>
2023-05-15 14:39:44 +0600

    g

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 function Btn(){
-    const clickHandler
-    return (
-        <button onClick={clickHandler}> 
-            Click Me 
-        </button>
-    )
+    // const clickHandler
+    // return (
+    //     <button onClick={clickHandler}> 
+    //         Click Me 
+    //     </button>
+    // )
 }
 
 export default Btn;
